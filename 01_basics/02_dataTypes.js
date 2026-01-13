5908ae87a5fdd771301e395ab419b0684228a215
HAID3R <120932421+hayd3rali@users.noreply.github.com>
2026-01-13 12:36:15 +0000

    Day 2 Start

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,1 +1,9 @@
-"use strict" // treat all js code as newer version.
+"use strict"; // treat all js code as newer version.
+
+// alert("hello") //we are usig node js not browser
+
+let name = "sam"
+let age = 20
+let isLoggedIn = false
+let state = null
+console.log(age);
